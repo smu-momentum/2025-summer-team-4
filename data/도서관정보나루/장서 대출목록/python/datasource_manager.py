596f3118a87ce428c82b80fa5fa04ec451bd7c48
Hepheir <hepheir@gmail.com>
2025-08-13 10:04:45 +0900

    chore(도서관정보나루): `DataSourceBatcher` 작성

---
+++ data/도서관정보나루/장서 대출목록/python/datasource_manager.py
@@ -0,0 +1,76 @@
+import collections
+import functools
+import logging
+import time
+
+import pandas as pd
+
+from config import DATASOURCE_FILE
+
+
+class DataSourceBatcher:
+    def __init__(self, logger: logging.Logger, max_timestamps: int = 10):
+        self.logger = logger
+        self._df = pd.read_csv(DATASOURCE_FILE)
+        self._timestamps = collections.deque()
+        self._max_timestamps = max_timestamps
+        self._record_batch_time()
+
+    def __del__(self):
+        del self._df
+
+    @functools.cache
+    def __len__(self) -> int:
+        self.logger.info(f'배치 크기를 측정하는 중...')
+        self.logger.info(f'총 탐색 대상 {len(self._df)}개.')
+        step = len(self._df)//100
+        size = 0
+        for i in range(len(self._df)):
+            if (i+1) % step == 0:
+                self.logger.info(f'배치 크기 탐색 {(i+1)//step}% 완료 ({i+1}/{len(self._df)})')
+            LibraryName, Year, Month, Url, ValidUrl, SaveAt = self._df.iloc[i]
+            if self.__filter__(i, LibraryName, Year, Month, Url, ValidUrl, SaveAt):
+                size += 1
+        return size
+
+    def __batch__(self, index: int, LibraryName: str, Year: int, Month: int, Url: str, ValidUrl: bool, SaveAt: str):
+        pass
+
+    def __filter__(self, index: int, LibraryName: str, Year: int, Month: int, Url: str, ValidUrl: bool, SaveAt: str) -> bool:
+        return True
+
+    def batch(self):
+        self.logger.info(f'배치 작업을 시작합니다.')
+        self.logger.info(f'배치 크기: {len(self)}')
+        count = 0
+        for i in range(len(self._df)):
+            LibraryName, Year, Month, Url, ValidUrl, SaveAt = self._df.iloc[i]
+            if self.__filter__(i, LibraryName, Year, Month, Url, ValidUrl, SaveAt):
+                self._record_batch_time()
+                count += 1
+                self.logger.info(f'배치 작업 진행 중: {count/len(self)*100:.1f}% ({count}/{len(self)})')
+                self.logger.info(f'예상 소요시간: {time_string(self._estimate_seconds_per_batch()*(len(self)-count))}')
+                self.__batch__(i, LibraryName, Year, Month, Url, ValidUrl, SaveAt)
+
+    def _record_batch_time(self):
+        self._timestamps.append(time.time())
+        while len(self._timestamps) > self._max_timestamps:
+            self._timestamps.popleft()
+
+    def _estimate_seconds_per_batch(self) -> float:
+        return (self._timestamps[-1]-self._timestamps[0]) / len(self._timestamps)
+
+
+def time_string(seconds: float) -> str:
+    """초 단위의 시간을 'x시간 x분 x초' 형식으로 변환"""
+    if seconds < 60:
+        return f'{seconds:.0f}초'
+    elif seconds < 3600:
+        minutes = int(seconds // 60)
+        seconds = int(seconds % 60)
+        return f'{minutes}분 {seconds}초'
+    else:
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        seconds = int(seconds % 60)
+        return f'{hours}시간 {minutes}분 {seconds}초'
